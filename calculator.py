467da0db4d856ca4430363c1f19470acd9253d8d
Hugo Bayona <46328014+hb4y@users.noreply.github.com>
2023-02-06 15:41:17 -0500

    Add and substract functions

--- calculator.py
+++ calculator.py
@@ -1,3 +1,11 @@
 """
 Basic calculator
 """
+
+
+def add(first_number, second_number):
+    return first_number + second_number
+
+
+def subtract(first_number, second_number):
+    return first_number - second_number
